5dae44651b3c692566cee72fe7e60358acf646ef
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-27 06:09:59 -0300

    add model exams

---
+++ models/exams.js
@@ -0,0 +1,21 @@
+const db = require('../db');
+
+module.exports = {
+    create: () => db.query(`
+        CREATE TYPE exam_type AS ENUM (
+            'analise clinica', 'imagem'
+        );
+        CREATE TYPE exam_status AS ENUM (
+            'ativo', 'inativo'
+        );
+
+        CREATE TABLE exams(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(512) NOT NULL,
+            type exam_type NOT NULL,
+            status exam_status DEFAULT 'ativo',
+        );
+
+        CREATE INDEX ON exams(type);
+    `),
+}
