455d78417231c30d627d65b847ba0ef18fa2408d
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-27 14:20:39 -0300

    fix examLaboratory model

--- models/examLaboratory.js
+++ models/examLaboratory.js
@@ -1,11 +1,15 @@
 const db = require('../db');
 
 module.exports = {
-    create: () => db.query(`
-        CREATE TABLE exam_laboratory(
-            exam_id int REFERENCES exam(id) ON UPDATE CASCADE ON DELETE CASCADE,
-            laboratory_id int REFERENCES laboratory(id) ON UPDATE CASCADE,
-            CONSTRAINT exam_laboratory_pkey PRIMARY KEY (exam_id, laboratory_id)
-        );
-    `),
+  create: () => db
+  // create table laboratory with the enum above as data type
+  .query(`
+    CREATE TABLE exam_laboratory(
+      exam_id int REFERENCES exam(id) ON UPDATE CASCADE ON DELETE CASCADE,
+      laboratory_id int REFERENCES laboratory(id) ON UPDATE CASCADE,
+      CONSTRAINT exam_laboratory_pkey PRIMARY KEY (exam_id, laboratory_id)
+    );
+  `)
+  .then(res => console.log('Successfuly created model exam_laboratory!'))
+  .catch(err => console.error('ERROR ON create TABLE exam_laboratory\n\n'+err.stack)),
 }
