ad022a26635d43bd4c6a9bf8ed037420bb07f4d3
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-27 14:22:10 -0300

    fix index.js db typo

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: 'dasatest',
-    host: 'localhost',
-    database: 'dasatest',
-    password: 'postgres@PSQL.123',
-    port: 5432,
-  })
+  user: 'dasatest',
+  host: 'localhost',
+  database: 'dasatest',
+  password: 'postgres@PSQL.123',
+  port: 5432,
+})
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
