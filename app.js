512848dc03c3ca465af501eebf0571d649364987
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-27 19:04:13 -0300

    add config/express.js to config structure

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-const bodyParser = require('body-parser')
-const compression = require('compression')
-const express = require('express')
+const app = require('./config/express')();
+const port = app.get('port')
 const routes = require('./routes');
-
-const app = express()
-const port = 3000
-
-
-
-app.use(bodyParser.json())
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-
-app.use(compression)
 
 routes(app)
 
---
+++ config/express.js
@@ -0,0 +1,21 @@
+const bodyParser = require('body-parser')
+const compression = require('compression')
+const express = require('express')
+
+module.exports = () => {
+  const app = express()
+
+  //set app variables
+  app.set('port', process.env.PORT || config.get('server.port'));
+
+  //use middlewares
+  app.use(bodyParser.json())
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  )
+  app.use(compression)
+
+  return app
+}
