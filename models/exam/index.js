83f6bf8441cdf9668bd081058264b285cfc526bc
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-28 13:09:14 -0300

    add select to exam model

--- models/exam/index.js
+++ models/exam/index.js
@@ -3,4 +3,5 @@ module.exports = {
   drop: require('./drop'),
   //the functions below returns a promise of the query
   insert: require('./insert'),
+  select: require('./select'),
 }
---
+++ models/exam/select.js
@@ -0,0 +1,21 @@
+/* query_terms expects to receive an object 
+which keys matches the column names, and values matches the values to search
+  example:
+    {
+      'name': 'blood analysis',
+      'status': 'active'
+    }
+  this will search for active blood analysis exams
+*/ 
+
+//this module returns a promise of the query
+module.exports = (db, query_terms) => {
+  let query = 'SELECT * FROM exam WHERE '
+  Object.keys(query_terms).forEach((key, i, arr) =>{
+    query += key+' = $'+i
+    if (i < arr.length) query += ' AND '
+  })
+  query += ';'
+  console.log(query);
+  return db.query(query, Object.values)
+}
