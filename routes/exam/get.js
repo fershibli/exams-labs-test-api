99d82cbffce60e4e29a40a0fe14344ea14436d19
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-28 18:20:01 -0300

    add exam get routes

---
+++ routes/exam/get.js
@@ -0,0 +1,16 @@
+const examControler = require('../../services/exam')
+
+module.exports = router => {
+  //list active exams
+  router.get('/active', (req, res) =>{
+    const { name, type, status } = req.body
+    examControler
+      .listActive()
+      .then(result => {
+        const activeExams = result.rows
+        return res.status(200).send(activeExams)
+      })
+      .catch(err => res.status(406).send(err.message))
+  })
+  //new exam post routes can be added below
+}
