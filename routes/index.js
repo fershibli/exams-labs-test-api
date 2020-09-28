86abfc401dfef86fc416a22ea2f23b4b74811fb6
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-28 19:37:58 -0300

    fix main routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 const examRoutes = require('./exam')
+const laboratoryRoutes = require('./laboratory')
+const examLaboratoryRoutes = require('./examLaboratory')
+
 
 module.exports = app => {
   examRoutes(app)
+  laboratoryRoutes(app)
+  examLaboratoryRoutes(app)
 }
