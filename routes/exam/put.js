a5b98f3c8495608d93e01deef63ed02e93e1dea5
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-28 18:20:17 -0300

    add put exam route

---
+++ routes/exam/put.js
@@ -0,0 +1,19 @@
+const examControler = require('../../services/exam')
+
+module.exports = router => {
+  //update existing exam
+  router.put('/:id', (req, res) =>{
+    const { name, type, status } = req.body
+    const id = parseInt(req.params.id)
+    if (id == null || id == undefined){
+      return res.status(406).send('must provide id to update')
+    }
+    examControler
+      .updateExam(id, name, type, status)
+      .then(result => {
+        return res.status(200).send(result.rows[0])
+      })
+      .catch(err => res.status(406).send(err.message))
+  })
+  //new exam post routes can be added below
+}
