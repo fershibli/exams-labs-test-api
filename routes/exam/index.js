68358e8cb943db8a6691488e272c49753c98d910
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-28 18:20:41 -0300

    add routes to examRouter

--- routes/exam/index.js
+++ routes/exam/index.js
@@ -1,10 +1,14 @@
 const { Router } = require('express')
+const examGetRoutes = require('./get')
 const examPostRoutes = require('./post')
+const examPutRoutes = require('./put')
 
 module.exports = app => {
   const examRouter = Router()
 
+  examGetRoutes(examRouter)
   examPostRoutes(examRouter)
+  examPutRoutes(examRouter)
 
   app.use('/exam', examRouter)
 }
