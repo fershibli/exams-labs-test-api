5129cb0176c8802b56656ffb597277a54df105f5
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-09-28 01:07:12 -0300

    add drop func to model laboratory

---
+++ db/models/laboratory/drop.js
@@ -0,0 +1,8 @@
+module.exports = db => db
+.query('DROP TABLE laboratory;')
+.then(res => db
+  .query('DROP TYPE laboratory_status;')
+  .then(res => console.log('Successfuly droped model laboratory!'))
+  .catch(err => console.error('ERROR ON drop TYPE laboratory_status\n\n'+err.stack))
+)
+.catch(err => console.error('ERROR ON drop TABLE laboratory\n\n'+err.stack))
